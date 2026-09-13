5bd47a4be999dd4534412770944d0f72d24ac8b5
Tapaswini Sahoo <tapaswinisahoo2005@gmail.com>
2026-09-13 22:06:20 +0530

    Fix Express 5 CORS wildcard route

--- server/index.js
+++ server/index.js
@@ -69,7 +69,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "x-requested-with"],
 }));
-app.options("*", cors());
+app.options(/.*/, cors());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
